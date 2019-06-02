f254a0560dd93cfc7ca22b3f0e38d0da90b3ffff
guihsabino <guilhermesabino0210@outlook.com>
2019-06-02 18:57:51 -0300

    commit

--- CadPro/CadPro/br.com.fatec.edu.cadpro.database/src/br/com/fatec/edu/cadpro/database/Teste.java
+++
@@ -1,5 +0,0 @@
-package br.com.fatec.edu.cadpro.database;
-
-public class Teste {
-
-}
--- CadPro/CadPro/br.com.fatec.edu.cadpro/src/br/com/fatec/edu/cadpro/it/ITProdutoPK.java
+++
@@ -1,6 +0,0 @@
-package br.com.fatec.edu.cadpro.it;
-
-public interface ITProdutoPK {
-
-	public int	getIdProduto();
-}
